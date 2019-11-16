35c67c1ff04f776c2e95f4924cc91d8c53c876fa
Uli Kusterer <ulivc@zathras.de>
2019-11-16 16:43:07 +0100

    Initial Commit

---
+++ AdairNetworking/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdairNetworking
+//
+//  Created by Uli Kusterer on 16.11.19.
+//  Copyright © 2019 Uli Kusterer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
